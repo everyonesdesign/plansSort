3a419cde4d2468ea8daff35a7f0d478e0ee954e2
Yura Trambitskiy <tr.yura@yahoo.com>
2014-07-30 20:55:45 +0400

    divided storage writing into 2 parts

--- dev/app.js
+++ dev/app.js
@@ -4,26 +4,26 @@
     if (!$("#content").length) return;
 
 //plan blocks
-    var $plans = $(".plan.todo"),
+    var $plans = $("[plan_id]"),
         $sortableContainer,
         opacityToSet = .4;
 
     $plans.dblclick(function (e) {
         if ($(this).css("opacity") == 1) $(this).css("opacity", opacityToSet);
         else $(this).css("opacity", 1);
-        writeDataToStorage();
-    }).wrapAll("<div class='all-plans-wrapper' id='sortablePlans'></div>");
+        writeAllToStorage();
+    }).wrapAll("<div id='sortablePlans'></div>");
 
     $sortableContainer = $("#sortablePlans");
     $sortableContainer.sortable({
-        stop: writeDataToStorage //on sortable complete renew data in storage
+        stop: writeAllToStorage //on sortable complete renew data in storage
     });
 
     if (localStorage["plansOrder"]) { //if order local storage is defined, reorder plans in the storage order. new plans will be positioned on top
         var orderArray = JSON.parse(localStorage["plansOrder"]);
         for (var i = 0; i < orderArray.length; i++) {
             $plans.each(function () {
-                if ($(this).children(".plan").attr("plan_id") == orderArray[i]) $(this).detach().appendTo($sortableContainer);
+                if ($(this).attr("plan_id") == orderArray[i]) $(this).detach().appendTo($sortableContainer);
             });
         }
     }
@@ -31,20 +31,29 @@
     if (localStorage["plansOpacity"]) { //if opacity local storage is defined apply it
         var opacityArray = JSON.parse(localStorage["plansOpacity"]);
         for (var i = 0; i < opacityArray.length; i++) {
-            $(".plan.todo[plan_id='" + opacityArray[i] + "']").parent().css("opacity", opacityToSet);
+            $plans.filter("[plan_id='" + opacityArray[i] + "']").css("opacity", opacityToSet);
         }
     }
 
-    writeDataToStorage();
-    function writeDataToStorage() { //function which saves current position to the storage
-        var orderArray = [],
-            opacityArray = [];
-        $(".plan-wrapper").each(function () {
-            var id = $(this).children(".plan").attr("plan_id");
+    writeAllToStorage();
+    function writeAllToStorage() { //save all the data
+        writeOrderToStorage();
+        writeOpacityToStorage();
+    }
+    function writeOrderToStorage() {
+        var orderArray = [];
+        $("[plan_id]").each(function () { //we have to reselect it to update the structure
+            var id = $(this).attr("plan_id");
             orderArray.push(id);
-            if ($(this).css("opacity") != 1) opacityArray.push(id);
         });
         localStorage["plansOrder"] = JSON.stringify(orderArray);
+    }
+    function writeOpacityToStorage() {
+        var opacityArray = [];
+        $plans.each(function () {
+            var id = $(this).attr("plan_id");
+            if ($(this).css("opacity") != 1) opacityArray.push(id);
+        });
         localStorage["plansOpacity"] = JSON.stringify(opacityArray);
     }
 
